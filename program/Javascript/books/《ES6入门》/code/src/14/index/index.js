d1b1569dafd76c6bde5d71a21956806f10712cb3
张春琳 <2413984950@qq.com>
2024-01-22 18:52:37 +0800

    《ES6入门》

--- program/Javascript/books/《ES6入门》/code/src/14/index/index.js
+++ program/Javascript/books/《ES6入门》/code/src/14/index/index.js
@@ -1,2 +1,5 @@
-const set8 = new Set(['red', 'green', 'blue'])
-set8.forEach((value, key, item) => console.log(key + ': ' + value, item))
+let set12 = new Set([1,2,3])
+set12 = new Set([...set12].map(item => item * 2)) // [2,4,6]
+console.log(set12)
+set12 = new Set(Array.from(set12, item => item * 2)) // [2,4,6]
+console.log(set12)
