4da41b83ce3d864734c42efb42fe1b05b25312db
张春琳 <2413984950@qq.com>
2024-01-25 09:00:36 +0800

    《ES6入门》

--- program/Javascript/books/《ES6入门》/code/src/14/index/index.js
+++ program/Javascript/books/《ES6入门》/code/src/14/index/index.js
@@ -1,6 +1,7 @@
-const ws = new WeakSet()
-// ws.add(1)
-ws.add(Symbol('test'))
-const a = {'name': 'sam'}
-ws.add(a).add(a)
-console.log(ws, ws.has(a))
+// 案例三： Map构造函数可接受的参数：Array/Set/Map 对象
+const array3 = [['id_01', {'name': 'sam'}], ['id_02', {'name': 'George'}]]
+const set3 = new Set(array3)
+const map3 = new Map(set3)
+const map3_1 = new Map(array3)
+const map3_2 = new Map(map3_1)
+console.info(array3,set3,map3,map3_1, map3_2)
